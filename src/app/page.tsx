ba423f86c01b440fb06fadec866a1bf7573c4cab
random776 <125573400+random776@users.noreply.github.com>
2025-05-02 12:30:03 +0900

    add explanation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <h1>地震波伝播シミュレーション（家庭版）</h1>
+      <p>画面をタップして震源位置を動かします。</p>
       <p>⚠️ 五月祭開始までこのurlを一般公開しないこと.</p>
       <WaveSpeedCanvas />
     </div>
