d81326418e24d490d604c0184a1e83ceacc9e2cc
random776 <125573400+random776@users.noreply.github.com>
2025-05-08 01:11:04 +0900

    非本質的な部分

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-gray-100 text-gray-600 text-sm py-6 border-t">
-      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col lg:flex-row justify-between items-center">
         <div className="space-box">
-          <h3 className="text-lg font-bold mb-2 md:mb-0">
+          <h3 className="text-lg font-bold mb-2 lg:mb-0">
             2025年 東京大学 理学部 地球惑星物理・環境学科有志
           </h3>
         </div>
